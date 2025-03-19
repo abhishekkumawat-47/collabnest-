ce754e1e0ff3bc5105a53a1d2843fcc468af713d
Arpit-Raj1 <arpitraj4114@gmail.com>
2025-03-20 02:31:46 +0530

    added auth

---
+++ app/api/auth/[...nextauth]/route.ts
@@ -0,0 +1,33 @@
+// src/app/api/auth/[...nextauth]/route.ts
+import NextAuth from 'next-auth';
+import AzureADProvider from 'next-auth/providers/azure-ad';
+import { NextAuthOptions } from 'next-auth';
+
+export const authOptions: NextAuthOptions = {
+    providers: [
+        AzureADProvider({
+            clientId: process.env.AZURE_AD_CLIENT_ID!,
+            clientSecret: process.env.AZURE_AD_CLIENT_SECRET!,
+            tenantId: process.env.AZURE_AD_TENANT_ID,
+            authorization: {
+                params: {
+                    scope: 'openid profile email',
+                },
+            },
+        }),
+    ],
+    callbacks: {
+        async signIn({ account, profile }) {
+            // ✅ Restrict to iitp.ac.in email
+            // return true;
+            if (profile?.email?.endsWith('@iitp.ac.in')) {
+                return true;
+            }
+            return false;
+        },
+    },
+};
+
+const handler = NextAuth(authOptions);
+
+export { handler as GET, handler as POST };
